8c08248fd9e5eb16853217debf97e9aa165b4b42
Jorgedc87 <73493938+Jorgedc87@users.noreply.github.com>
2021-04-16 22:15:11 -0300

    corregido ejercicio 3

--- app/ejercicio3.php
+++ app/ejercicio3.php
@@ -2,46 +2,20 @@
 
 $mes =  rand(1, 12); 
 
-echo "La variable mes es " . $mes . " y el mes es ";
+echo "La variable mes es " . $mes . " y la estación es ";
 
-switch($mes){
-    case '1': 
-        echo "enero";
-        break;
-    case '2': 
-        echo "febrero";
-        break;
-    case '3': 
-        echo "marzo";
-        break;
-    case '4': 
-        echo "abril";
-        break;
-    case '5': 
-        echo "mayo";
-        break;
-    case '6': 
-        echo "junio";
-        break;
-    case '7': 
-        echo "julio";
-        break;
-    case '8': 
-        echo "agosto";
-        break;
-    case '9': 
-        echo "septiembre";
-        break;
-    case '10': 
-        echo "octubre";
-        break;
-    case '11': 
-        echo "noviembre";
-        break;
-    case '12': 
-        echo "diciembre";
-        break;
-        
+if($mes>0 && $mes<=3){
+    echo "verano";
+}else{
+    if($mes>3 && $mes<=6){
+        echo "otoño";
+    }else{
+        if($mes>6 && $mes<=9){
+            echo "invierno";
+        }else{
+            echo "primavera";
+        }
+    }
 }
 
 ?>
