e23d0ed0da4c00efc68989c2992b30b54e8f986e
Jorgedc87 <73493938+Jorgedc87@users.noreply.github.com>
2021-04-23 19:58:11 -0300

    Realizado el ejercicio 5

--- app/ejercicio5.php
+++ app/ejercicio5.php
@@ -16,53 +16,23 @@ if (isset($_POST['edad'])){
 if (!empty($edad)){
     switch($edad){
         case 1:
-            $resultado = "es menor de edad";
-            break; 
         case 2:
-            $resultado = "es menor de edad";
-            break; 
         case 3:
-            $resultado = "es menor de edad";
-            break; 
         case 4:
-            $resultado = "es menor de edad";
-            break; 
         case 5:
-            $resultado = "es menor de edad";
-            break; 
         case 6:
-            $resultado = "es menor de edad";
-            break; 
         case 7:
-            $resultado = "es menor de edad";
-            break; 
         case 8:
-            $resultado = "es menor de edad";
-            break; 
         case 9:
-            $resultado = "es menor de edad";
-            break; 
         case 10:
-            $resultado = "es menor de edad";
-            break; 
         case 11:
-            $resultado = "es menor de edad";
-            break; 
         case 12:
             $resultado = "es menor de edad";
             break; 
         case 13:
-            $resultado = "es adolescente";
-            break; 
         case 14:
-            $resultado = "es adolescente";
-            break; 
         case 15:
-            $resultado = "es adolescente";
-            break; 
         case 16:
-            $resultado = "es adolescente";
-            break; 
         case 17:
             $resultado = "es adolescente";
             break; 
